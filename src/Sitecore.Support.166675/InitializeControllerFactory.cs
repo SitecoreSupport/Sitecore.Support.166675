f2feaa98c7c28a7e94ed348c44d9df132da64252
DK\mnas <mnas@sitecore.net>
2017-06-09 10:12:18 +0800

    Commit Patch

---
+++ src/Sitecore.Support.166675/InitializeControllerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines;
+using System.Web.Mvc;
+using Sitecore.Support.Mvc.Controllers;
+
+namespace Sitecore.Support.Mvc.Pipelines.Loader
+{
+    public class InitializeControllerFactory
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            this.SetControllerFactory(args);
+        }
+
+        protected virtual void SetControllerFactory(PipelineArgs args)
+        {
+            var controllerFactory = new SitecoreControllerFactoryExtended(ControllerBuilder.Current.GetControllerFactory());
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+    }
+}
---
+++ src/Sitecore.Support.166675/SitecoreControllerFactory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Configuration;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Helpers;
+
+namespace Sitecore.Support.Mvc.Controllers
+{
+    public class SitecoreControllerFactoryExtended : SitecoreControllerFactory
+    {
+        public SitecoreControllerFactoryExtended(IControllerFactory innerFactory) : base(innerFactory)
+        {
+        }
+
+        public override IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            Assert.ArgumentNotNull(requestContext, "requestContext");
+            Assert.ArgumentNotNull(controllerName, "controllerName");
+            try
+            {
+                HttpContextBase requiredService = ServiceLocator.ServiceProvider.GetRequiredService<HttpContextBase>();
+                ISitecoreServiceLocatorScope scope = requiredService.Items[typeof(ISitecoreServiceLocatorScope)] as ISitecoreServiceLocatorScope;
+
+                if (controllerName.EqualsText(this.SitecoreControllerName))
+                {
+                    return this.CreateSitecoreController(requestContext, controllerName);
+                }
+                DefaultControllerFactory factory = this.InnerFactory as DefaultControllerFactory;
+                if (factory != null)
+                {
+                    //Get controller type
+                    MethodInfo dynMethod = factory.GetType().GetMethod("GetControllerType", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    Type serviceType = (Type)dynMethod.Invoke(factory, new object[] { requestContext, controllerName });
+
+                    if (serviceType != null)
+                    {
+                        if (scope != null)
+                        {
+                            var controller = (scope.ServiceProvider.GetService(serviceType) as IController) ?? TypeHelper.CreateObject<IController>(serviceType, new object[0]);
+                            return (controller ?? this.InnerFactory.CreateController(requestContext, controllerName));
+                        }
+                    }
+                }
+                return this.InnerFactory.CreateController(requestContext, controllerName);
+
+            }
+            catch (Exception exception)
+            {
+                if (!MvcSettings.DetailedErrorOnMissingController)
+                {
+                    throw;
+                }
+                throw this.CreateControllerCreationException(controllerName, exception);
+            }
+        }
+    }
+}
